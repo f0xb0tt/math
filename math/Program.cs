a90a916689f362bcb0a758e8d38e499dcf46943b
f0xb0tt <foxplaystoomuch@gmail.com>
2025-07-29 14:42:31 +0300

    added files

---
+++ math/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("count to:");
+        int input = int.Parse(Console.ReadLine());
+        BigInteger count = (BigInteger)Math.Pow(10,input);
+        DateTime dateTime = DateTime.Now;
+        for (BigInteger i = 0; i <= count; i++)
+        {
+            //Console.WriteLine($"{i.ToString()}");
+        }
+        Console.WriteLine($"counted to {count} in {(DateTime.Now - dateTime).TotalHours} hours");
+    }       
+}
